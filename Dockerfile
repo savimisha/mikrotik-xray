f4e5aa203cdaff8093e337212c0574435925f94b
Mikhail Savichev <mikhail@savichev.pro>
2026-02-22 02:55:27 +0400

    Refactor build. Added ARM.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,24 @@
-FROM alpine:latest
+FROM alpine:latest AS build
+ARG TARGETPLATFORM
+ARG XRAY_ARM="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm32-v7a.zip"
+ARG XRAY_ARM64="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip"
 
 RUN set ex;
 
+RUN apk add --no-cache curl unzip
+
+RUN if [[ "$TARGETPLATFORM" == "linux/arm64" ]]; then \
+    curl -L -o /xray.zip $XRAY_ARM64; else \
+    curl -L -o /xray.zip $XRAY_ARM; fi
+
+RUN unzip /xray.zip
+
+FROM alpine:latest
+
 WORKDIR /app
 
-COPY ./xray /app
+COPY --from=build /xray /app/xray
+
 COPY ./entrypoint.sh /app
 
 RUN chmod +x /app/xray
--- build.sh
+++ build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-XRAY_LINK="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip"
-
-sudo apt -y install unzip curl
-rm xray
-mkdir tmp
-curl -L -o tmp/xray.zip $XRAY_LINK
-cd tmp
-unzip xray.zip
-cp xray ../xray
-cd ..
-rm -rf tmp/
-
 docker run --privileged --rm tonistiigi/binfmt --install all
-docker buildx build -f Dockerfile --no-cache --progress=plain --platform linux/arm64 --output=type=docker --tag savimisha/mikrotik-xray .
-docker push savimisha/mikrotik-xray
+docker buildx build -f Dockerfile --no-cache --progress=plain --platform linux/arm64,linux/arm --output=type=docker --tag savimisha/mikrotik-xray:latest .
+docker push savimisha/mikrotik-xray:latest
